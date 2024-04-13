4751f9665c0ce3df6f23f8bb5aca2b6ca9af731b
ip13 <iperevozchikov2002@gmail.com>
2024-04-13 17:21:16 +0300

    first commit

---
+++ src/main/kotlin/com/ip13/main/Main.kt
@@ -0,0 +1,14 @@
+package com.ip13.main
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+class Main
+
+fun main(args: Array<String>) {
+    runApplication<Main>(*args)
+}
